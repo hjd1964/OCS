fc4773c45bb8f0a29ec6afafb48f01161d4c5fe6
Howard Dutton <hjd1964@gmail.com>
2017-06-06 08:32:05 -0400

    Updated cloudiness descriptions

--- Misc.ino
+++ Misc.ino
@@ -43,15 +43,15 @@ String weatherCloudCoverDescriptionShort() {
   double s=getAvgSkyDiffTemp();
   if (s <= -200) return "Invalid"; else
   if (s <= WEATHER_VCLR_THRESHOLD) return "Clear"; else
-  if (s <= WEATHER_CLER_THRESHOLD) return "Mostly Clr"; else
-  if (s <= WEATHER_HAZE_THRESHOLD) return "Slight Hze"; else
+  if (s <= WEATHER_CLER_THRESHOLD) return "Mostly-Clr"; else
+  if (s <= WEATHER_HAZE_THRESHOLD) return "Slight-Hze"; else
   if (s <= (WEATHER_HAZE_THRESHOLD+WEATHER_OVRC_THRESHOLD)/2.0) return "Ovrcst/Hze"; else
   if (s <= WEATHER_OVRC_THRESHOLD) return "Overcast"; else
   if (s <= (WEATHER_OVRC_THRESHOLD+WEATHER_CLDY_THRESHOLD)/2.0) return "Cld/Ovrcst"; else
-  if (s <= WEATHER_CLDY_THRESHOLD) return "Mstly Cldy"; else
+  if (s <= WEATHER_CLDY_THRESHOLD) return "Mstly-Cldy"; else
   if (s <= (WEATHER_CLDY_THRESHOLD+WEATHER_VCLD_THRESHOLD)/2.0) return "Cloudy"; else
-  if (s <= WEATHER_VCLD_THRESHOLD) return "Very Cldy"; else
-  if (s >  WEATHER_VCLD_THRESHOLD) return "Very Cldy";
+  if (s <= WEATHER_VCLD_THRESHOLD) return "Very-Cldy"; else
+  if (s >  WEATHER_VCLD_THRESHOLD) return "Very-Cldy";
 #else
   return "N/A";
 #endif
