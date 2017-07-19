72e8702fd8d70c983cbb1025f82f74f8903a9155
Howard Dutton <hjd1964@gmail.com>
2017-07-19 10:09:29 -0400

    Ignore NTP requests if roof is moving

--- NTP.ino
+++ NTP.ino
@@ -8,6 +8,9 @@ byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
 
 time_t getNtpTime()
 {
+  // no updates if the roof is moving
+  if (roofState!='i') return 0;
+  
   unsigned long tOut=millis()+3000;
   while ((Udp.parsePacket() > 0) && (millis() - tOut < 0)) ; // discard any previously received packets
 #ifdef NTP_DEBUG_ON
