d09bd044c8dd3692526a00990d21ac374e0cca19
Howard Dutton <hjd1964@gmail.com>
2017-08-14 08:01:06 -0400

    Fixed typo in comment

--- Cloud.ino
+++ Cloud.ino
@@ -118,8 +118,8 @@ void clouds(void) {
           dtostrf2(lad,5,1,-99.9,999.9,temp);                  dataFile.write(" "); dataFile.write(temp); //19, 6 (long term average dif (sky) temperature)
           dtostrf2(p,6,1,-999.9,9999.9,temp);                  dataFile.write(" "); dataFile.write(temp); //25, 7 (pressure)
           dtostrf2(h,5,1,-99.9,999.9,temp);                    dataFile.write(" "); dataFile.write(temp); //32, 6 (humidity)
-          dtostrf2(weatherWindspeed(),5,1,-99.9,999.9,temp);   dataFile.write(" "); dataFile.write(temp); //38, 5 (windspeed)
-          dtostrf2(weatherSkyQuality(),5,2,-9.99,999.99,temp); dataFile.write(" "); dataFile.write(temp); //44, 5 (sky quality)
+          dtostrf2(weatherWindspeed(),5,1,-99.9,999.9,temp);   dataFile.write(" "); dataFile.write(temp); //38, 6 (windspeed)
+          dtostrf2(weatherSkyQuality(),5,2,-9.99,999.99,temp); dataFile.write(" "); dataFile.write(temp); //44, 6 (sky quality)
           for (int i=0; i<29; i++) dataFile.write(" ");                                                   //  ,29
           dataFile.write("\r\n");                                                                         //  , 2
           dataFile.close();
