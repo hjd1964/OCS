d1f5cf7f8a5ce7a61d35ff47bd444097eee032e9
Howard Dutton <hjd1964@gmail.com>
2020-01-13 21:09:35 -0500

    Upped minor revision

--- OCS.ino
+++ OCS.ino
@@ -36,7 +36,7 @@
 // firmware info
 #define ONCUE_OCS
 #define FirmwareDate   __DATE__
-#define FirmwareNumber "1.4f"
+#define FirmwareNumber "1.5a"
 #define FirmwareName   "OnCue OCS"
 #define FirmwareTime   __TIME__
 
