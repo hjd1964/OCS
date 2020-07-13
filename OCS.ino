8e1d8e9e6babaaaf7e551e73cf0b81ca38625b57
Howard Dutton <hjd1964@gmail.com>
2020-07-13 12:58:06 -0400

    Upped patch level

--- OCS.ino
+++ OCS.ino
@@ -36,7 +36,7 @@
 // firmware info
 #define ONCUE_OCS
 #define FirmwareDate   __DATE__
-#define FirmwareNumber "2.3b"
+#define FirmwareNumber "2.3c"
 #define FirmwareName   "OnCue OCS"
 #define FirmwareTime   __TIME__
 
