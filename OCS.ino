71fbef9f7b94c3cab26ea62c46ec182a4ed81e47
Howard Dutton <hjd1964@gmail.com>
2022-04-07 12:53:02 -0400

    Upped minor revision

--- OCS.ino
+++ OCS.ino
@@ -38,8 +38,8 @@
 // Firmware version ----------------------------------------------------------------------------------------------------------------
 #define FirmwareName                "OCS"
 #define FirmwareVersionMajor        3
-#define FirmwareVersionMinor        02     // minor version 00 to 99
-#define FirmwareVersionPatch        "b"    // for example major.minor patch: 10.03c
+#define FirmwareVersionMinor        03     // minor version 00 to 99
+#define FirmwareVersionPatch        "a"    // for example major.minor patch: 10.03c
 #define FirmwareVersionConfig       2      // internal, for tracking configuration file changes
 
 #include "src/Common.h"
