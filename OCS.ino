7dff41804a97d878175bad083da9959bb2d7d1ae
Howard Dutton <hjd1964@gmail.com>
2023-01-26 15:11:05 -0500

    Upped patch level

--- OCS.ino
+++ OCS.ino
@@ -2,7 +2,7 @@
  * Title       Observatory-Control-System
  * by          Howard Dutton
  *
- * Copyright (C) 2012 to 2022 Howard Dutton
+ * Copyright (C) 2012 to 2023 Howard Dutton
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@
 #define FirmwareName                "OCS"
 #define FirmwareVersionMajor        3
 #define FirmwareVersionMinor        03     // minor version 00 to 99
-#define FirmwareVersionPatch        "h"    // for example major.minor patch: 10.03c
+#define FirmwareVersionPatch        "i"    // for example major.minor patch: 10.03c
 #define FirmwareVersionConfig       2      // internal, for tracking configuration file changes
 
 #include "src/Common.h"
