3ca6c288d2d3fda34736e32463646bf161bc9b91
Howard Dutton <hjd1964@gmail.com>
2023-05-12 11:51:48 -0400

    Only show navigation for active pages

--- src/pages/Page.cpp
+++ src/pages/Page.cpp
@@ -15,13 +15,17 @@ void pageHeader(int selected) {
   if (selected == PAGE_INDEX) { strcpy_P(temp, html_links_selected); www.sendContent(temp); }
   strcpy_P(temp, html_links_index_end); www.sendContent(temp);
 
-  strcpy_P(temp, html_links_weather_begin); www.sendContent(temp);
-  if (selected == PAGE_WEATHER) { strcpy_P(temp, html_links_selected); www.sendContent(temp); }
-  strcpy_P(temp, html_links_weather_end); www.sendContent(temp);
+  #if WEATHER == ON && WEATHER_CHARTS == ON
+    strcpy_P(temp, html_links_weather_begin); www.sendContent(temp);
+    if (selected == PAGE_WEATHER) { strcpy_P(temp, html_links_selected); www.sendContent(temp); }
+    strcpy_P(temp, html_links_weather_end); www.sendContent(temp);
 
-  strcpy_P(temp, html_links_sky_begin); www.sendContent(temp);
-  if (selected == PAGE_SKY_CONDITIONS) { strcpy_P(temp, html_links_selected); www.sendContent(temp); }
-  strcpy_P(temp, html_links_sky_end); www.sendContent(temp);
+    #if WEATHER_SKY_QUAL == ON || WEATHER_CLOUD_CVR == ON 
+      strcpy_P(temp, html_links_sky_begin); www.sendContent(temp);
+      if (selected == PAGE_SKY_CONDITIONS) { strcpy_P(temp, html_links_selected); www.sendContent(temp); }
+      strcpy_P(temp, html_links_sky_end); www.sendContent(temp);
+    #endif
+  #endif
 
   strcpy_P(temp, html_page_header_end); www.sendContent(temp);
 }
