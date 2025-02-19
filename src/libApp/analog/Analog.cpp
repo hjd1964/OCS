f90ef2839a91f2ace1da62e85963760289782bc5
Howard Dutton <hjd1964@gmail.com>
2025-02-19 14:36:14 -0500

    Better analog read for ESP32

--- src/libApp/analog/Analog.cpp
+++ src/libApp/analog/Analog.cpp
@@ -4,7 +4,13 @@
 
 float Analog::read(int n) {
   if (n < 1 || n > 16) return NAN;
-  if (pins[n - 1] != OFF) return analogRead(pins[n - 1])/1023.0F; else return NAN;
+  if (pins[n - 1] != OFF) {
+    #if ESP32
+      return analogReadMilliVolts(pins[n - 1])/3300.0F;
+    #else
+      return analogRead(pins[n - 1])/1023.0F;
+    #endif
+  } else return NAN;
 }
 
 Analog analog;
