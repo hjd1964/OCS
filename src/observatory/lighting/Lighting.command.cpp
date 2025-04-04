cf100a188a45b4f57ab02c7edd2dd0e00f27796e
Howard Dutton <hjd1964@gmail.com>
2025-04-04 14:19:45 -0400

    Allow light commands even without a lighting panel

--- src/observatory/lighting/Lighting.command.cpp
+++ src/observatory/lighting/Lighting.command.cpp
@@ -8,51 +8,49 @@
 bool Lighting::command(char reply[], char command[], char parameter[], bool *supressFrame, bool *numericReply, CommandError *commandError) {
   UNUSED(supressFrame);
 
-  #if LIGHT != OFF
-    //  :GLW#        Get observing room light mode
-    //               Returns OFF#, DIM#, RED#, or WHITE#
-        if (command[0] == 'G' && command[1] == 'L' && parameter[0] == 'W' && parameter[1] == 0) {
-      if (get(LL_WARM_ROOM) == LM_OFF) strcpy(reply, "OFF"); else
-      if (get(LL_WARM_ROOM) == LM_DIM_RED) strcpy(reply, "DIM"); else
-      if (get(LL_WARM_ROOM) == LM_RED) strcpy(reply, "RED"); else
-      if (get(LL_WARM_ROOM) == LM_WHITE) strcpy(reply, "WHITE");
-      *numericReply = false;
-    } else
+  //  :GLW#        Get observing room light mode
+  //               Returns OFF#, DIM#, RED#, or WHITE#
+      if (command[0] == 'G' && command[1] == 'L' && parameter[0] == 'W' && parameter[1] == 0) {
+    if (get(LL_WARM_ROOM) == LM_OFF) strcpy(reply, "OFF"); else
+    if (get(LL_WARM_ROOM) == LM_DIM_RED) strcpy(reply, "DIM"); else
+    if (get(LL_WARM_ROOM) == LM_RED) strcpy(reply, "RED"); else
+    if (get(LL_WARM_ROOM) == LM_WHITE) strcpy(reply, "WHITE");
+    *numericReply = false;
+  } else
 
-    //  :GLO#        Get observing room light mode
-    //               Returns OFF#, DIM#, RED#, or WHITE#
-    if (command[0] == 'G' && command[1] == 'L' && parameter[0] == 'O' && parameter[1] == 0) {
-      if (get(LL_OBSERVING_ROOM) == LM_OFF) strcpy(reply, "OFF"); else
-      if (get(LL_OBSERVING_ROOM) == LM_DIM_RED) strcpy(reply, "DIM"); else
-      if (get(LL_OBSERVING_ROOM) == LM_RED) strcpy(reply, "RED"); else
-      if (get(LL_OBSERVING_ROOM) == LM_WHITE) strcpy(reply, "WHITE");
-      *numericReply = false;
-    } else
+  //  :GLO#        Get observing room light mode
+  //               Returns OFF#, DIM#, RED#, or WHITE#
+  if (command[0] == 'G' && command[1] == 'L' && parameter[0] == 'O' && parameter[1] == 0) {
+    if (get(LL_OBSERVING_ROOM) == LM_OFF) strcpy(reply, "OFF"); else
+    if (get(LL_OBSERVING_ROOM) == LM_DIM_RED) strcpy(reply, "DIM"); else
+    if (get(LL_OBSERVING_ROOM) == LM_RED) strcpy(reply, "RED"); else
+    if (get(LL_OBSERVING_ROOM) == LM_WHITE) strcpy(reply, "WHITE");
+    *numericReply = false;
+  } else
 
-    //  :SLW,[mode]# Set warm room light [mode] = OFF, DIM, RED, WHITE 
-    //               Example: :SLW,OFF#
-    //               Returns: 1 on success, 0 on failure
-    if (command[0] == 'S' && command[1] == 'L' && parameter[0] == 'W' && parameter[1] == ',') {
-      String ws = String(parameter);
-      ws = ws.substring(2);
-      if (ws.equals("OFF")) set(LL_WARM_ROOM, LM_OFF); else
-      if (ws.equals("DIM")) set(LL_WARM_ROOM, LM_DIM_RED); else
-      if (ws.equals("RED")) set(LL_WARM_ROOM, LM_RED); else
-      if (ws.equals("WHITE")) set(LL_WARM_ROOM, LM_WHITE); else *commandError = CE_PARAM_FORM;
-    } else
+  //  :SLW,[mode]# Set warm room light [mode] = OFF, DIM, RED, WHITE 
+  //               Example: :SLW,OFF#
+  //               Returns: 1 on success, 0 on failure
+  if (command[0] == 'S' && command[1] == 'L' && parameter[0] == 'W' && parameter[1] == ',') {
+    String ws = String(parameter);
+    ws = ws.substring(2);
+    if (ws.equals("OFF")) set(LL_WARM_ROOM, LM_OFF); else
+    if (ws.equals("DIM")) set(LL_WARM_ROOM, LM_DIM_RED); else
+    if (ws.equals("RED")) set(LL_WARM_ROOM, LM_RED); else
+    if (ws.equals("WHITE")) set(LL_WARM_ROOM, LM_WHITE); else *commandError = CE_PARAM_FORM;
+  } else
 
-    //  :SLO,[mode]# Set observing room light [mode] = OFF, DIM, RED, WHITE 
-    //               Example: :SLO,RED#
-    //               Returns: 1 on success, 0 on failure
-    if (command[0] == 'S' && command[1] == 'L' && parameter[0] == 'O' && parameter[1] == ',') {
-      String ws = String(parameter);
-      ws = ws.substring(2);
-      if (ws.equals("OFF")) set(LL_OBSERVING_ROOM, LM_OFF); else
-      if (ws.equals("DIM")) set(LL_OBSERVING_ROOM, LM_DIM_RED); else
-      if (ws.equals("RED")) set(LL_OBSERVING_ROOM, LM_RED); else
-      if (ws.equals("WHITE")) set(LL_OBSERVING_ROOM, LM_WHITE); else *commandError = CE_PARAM_FORM;
-    } else
-  #endif
+  //  :SLO,[mode]# Set observing room light [mode] = OFF, DIM, RED, WHITE 
+  //               Example: :SLO,RED#
+  //               Returns: 1 on success, 0 on failure
+  if (command[0] == 'S' && command[1] == 'L' && parameter[0] == 'O' && parameter[1] == ',') {
+    String ws = String(parameter);
+    ws = ws.substring(2);
+    if (ws.equals("OFF")) set(LL_OBSERVING_ROOM, LM_OFF); else
+    if (ws.equals("DIM")) set(LL_OBSERVING_ROOM, LM_DIM_RED); else
+    if (ws.equals("RED")) set(LL_OBSERVING_ROOM, LM_RED); else
+    if (ws.equals("WHITE")) set(LL_OBSERVING_ROOM, LM_WHITE); else *commandError = CE_PARAM_FORM;
+  } else
 
   return false;
   
